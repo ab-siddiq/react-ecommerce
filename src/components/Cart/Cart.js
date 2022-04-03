7f54c4a25dcb16072f556ba95f4fbdacaa536851
siddiq <siddiq.aiub@gmail.com>
2022-04-03 16:45:25 +0600

    added add cart calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({cart}) => {
-    console.log(cart);
+    
    
     let totalPrice = 0;
+    let shipping = 0;
+    let tax = 0;
+    let grandTotal = 0;
+    for (const product of cart) {
+        totalPrice = totalPrice + product.price;
+        shipping = shipping + product.shipping;
+    }
+    tax = (totalPrice * .2).toFixed(2);
+    grandTotal = totalPrice + shipping + tax;
    
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Product: {cart.length}</p>
-            <p>Total Price: {}</p>
-            <p>Shipping Price: </p>
-            <p>Tax: </p>
-            <p>Grand Total: </p>
+            <p>Total Price: {totalPrice}</p>
+            <p>Shipping Price: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: {grandTotal}</p>
         </div>
     );
 };
